dda9a59fc4e1bd24811f413cfa025f6eb6f957ce
Andy Williams <andy@andywilliams.me>
2017-10-04 23:52:29 +0100

    Fix clang lookup dirs on osx

--- scripts/clang_include_dir.sh
+++ scripts/clang_include_dir.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-clang -E - -v < /dev/null 2>&1 | grep "^ /" | grep clang
+BINDIR=
+if [[ -d /usr/local/opt/llvm ]]; then
+  BINDIR="/usr/local/opt/llvm/bin/"
+fi
+
+${BINDIR}clang -E - -v < /dev/null 2>&1 | grep "^ /" | grep clang
