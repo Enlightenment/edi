51d81383b15a6d8892a3251080eb7dad957b0a38
Alastair Poole <netstar@gmail.com>
2018-08-19 19:41:16 +0100

    filepanel: remove bogus minsize.
    This fixes the jumping of the UI on tab change.

--- src/bin/edi_filepanel.c
+++ src/bin/edi_filepanel.c
@@ -1201,7 +1201,6 @@ edi_filepanel_add(Evas_Object *parent, Evas_Object *win,
    elm_genlist_homogeneous_set(list, EINA_TRUE);
    elm_genlist_select_mode_set(list, ELM_OBJECT_SELECT_MODE_ALWAYS);
    elm_genlist_filter_set(list, "");
-   evas_object_size_hint_min_set(list, 100, -1);
    evas_object_size_hint_weight_set(list, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
    evas_object_size_hint_align_set(list, EVAS_HINT_FILL, EVAS_HINT_FILL);
    evas_object_show(list);
